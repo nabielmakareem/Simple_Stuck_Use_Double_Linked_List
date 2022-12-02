9346b43e1dbbe1589d095e38d46a8b919f100000
nabielmakareem <nabielmakareem10@gmail.com>
2022-12-02 16:14:25 +0700

    add namespace

--- Simple_Stuck_Use_Double_Linked_List/Program.cs
+++ Simple_Stuck_Use_Double_Linked_List/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace Simple_Stuck_Use_Double_Linked_List_CSharp
+{
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+}
